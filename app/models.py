d6e2524be86ff748c7a90f798a57bd4a1b349428
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-08 01:04:15 +0300

    create user model

---
+++ app/models.py
@@ -0,0 +1,46 @@
+import email
+import profile
+
+from sqlalchemy import delete
+from . import db, login_manager
+from flask_login import UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+class User(db.Model, UserMixin):
+    __tablename__="users"
+    id=db.Column(db.Integer, Primary_Key=True)
+    username=db.Column(db.String(255), Unique=True, nullable=False)
+    email=db.Column(db.String(255), Unique=True, nullable=False)
+    secure_password=db.Column(db.String(255), nullable=False)
+    bio=db.Column(db.String(255))
+    profile_pic_path=db.Column(db.String())
+    pitches=db.relationship("pitch",backref="user",lazy="dynamic")
+    downvotes=db.relationship("downvote",backref="user",lazy="dynamic")
+    upvotes=db.relationship("upvote",backref="user",lazy="dynamic")
+    @property
+    def set_password(self):
+        raise AttributeError("You cannot read the password attribute")
+
+    @set_password.setter
+    def password(self,password):
+        self.secure_password=generate_password_hash(password)
+    
+    # def verify_password(self,password):
+    #     return check_password_hash(self.secure_password,password)
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"user:{self.username}"
+        
+
+
+
+
