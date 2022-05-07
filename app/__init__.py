a121b7c0152936018d6d134b511265e886ee29de
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-07 16:39:21 +0300

    import auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-from cgitb import strong
-from ensurepip import bootstrap
 from flask import Flask
 import sqlalchemy
 from config import config_options
@@ -17,4 +15,14 @@ login_manager.session_protection="strong"
 login_manager.login_view="auth.login"
 photos=UploadSet("photos",IMAGES)
 
-def create_app(config_name)
+def create_app(config_name):
+    app=Flask(__name__)
+    app.config.from_object(config_options[config_name])
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    configure_uploads(app,photos)
+    
+    return app
