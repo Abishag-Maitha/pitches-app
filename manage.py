a4e15308a17dce5ec3bb25ebdbd1ca1bb9d2fde0
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-07 16:59:33 +0300

    import manager and server

---
+++ manage.py
@@ -0,0 +1,15 @@
+from app import create_app,db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
+app=create_app("development")
+manager=Manager(app)
+manager.add_command("run",Server(use_debugger=True))
+manager.add_command("db", MigrateCommand)
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db)
+    
+if __name__=="__main__":
+    manager.run()
+
