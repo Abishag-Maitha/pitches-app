4d769f2b3dbecc9a88ff91f7c0942f498ab8ee69
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-07 16:43:57 +0300

    import main and auth blueprints into app

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,11 @@ photos=UploadSet("photos",IMAGES)
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
 
     login_manager.init_app(app)
     db.init_app(app)
