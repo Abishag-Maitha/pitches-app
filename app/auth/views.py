c3526bdb99a55c2602177256b77f7d8606761417
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-08 23:39:42 +0300

    create base.html file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,20 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import auth
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/sign_up',methods = ["GET","POST"])
+def sign_up():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/sign_up.html',registration_form = form)
