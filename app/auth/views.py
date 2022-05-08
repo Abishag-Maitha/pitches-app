8bbdf19611aa3c267d254165d301e3665a7d9d24
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-09 00:11:56 +0300

    create login route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm,RegistrationForm
 from .. import db
+
 
 @auth.route('/login')
 def login():
@@ -18,3 +20,17 @@ def sign_up():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/sign_up.html',registration_form = form)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+    if user is not None and user.verify_password(login_form.password.data):
+        login_user(user,login_form.remember.data)
+        return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "pitches login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
