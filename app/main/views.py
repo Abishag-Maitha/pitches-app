9599c9217f6aa20580fee048ed2c30cc2b7cc19d
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-09 14:21:51 +0300

    Create a route that will process the form submission request

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from flask import render_template,request,redirect,url_for,abort
 from app import main
 from ..models import Reviews, User
 from .forms import ReviewForm,UpdateProfile
-from .. import db
+from .. import db,photos
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -11,7 +11,6 @@ def profile(uname):
 
     if user is None:
         abort(404)
-
     return render_template("profile/profile.html", user = user)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
@@ -30,5 +29,16 @@ def update_profile(uname):
         db.session.commit()
 
         return redirect(url_for('.profile',uname=user.username))
-
     return render_template('profile/update.html',form =form)
+
+    
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
