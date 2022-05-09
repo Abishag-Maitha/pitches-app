c0d49eeab08d026a03870816387f559b027fc0de
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-09 15:57:55 +0300

    create downvote and comment classes

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-
+  
     @property
     def set_password(self):
         raise AttributeError("You cannot read the password attribute")
@@ -55,6 +55,13 @@ class Pitch(db.Model):
     downvotes=db.relationship("downvote",backref="pitch",lazy="dynamic")
     upvotes=db.relationship("upvote",backref="pitch",lazy="dynamic")
 
+def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+def __repr__(self):
+        return f"pitch:{self.post}" 
+
 class Upvote(db.Model):
     __tablename__="upvotes"
     id=db.Column(db.Integer, Primary_Key=True)
@@ -65,13 +72,53 @@ def save(self):
     db.session.add(self)
     db.session.commit()
 
+def __repr__(self):
+        return f"vote:{self.user_id}:{self.pitch_id}"
+
 @classmethod
 def display_upvotes(cls,id):
     upvote=Upvote.query.filter_by(pitch_id=id).all()
 
     return upvote
 
+class Downvote(db.Model):
+    __tablename__="downvotes"
+    id=db.Column(db.Integer, Primary_Key=True)
+    user_id=db.Column(db.Integer, ForeignKey('users.id'))
+    pitch_id=db.Column(db.Integer, ForeignKey('pitches.id'))
 
+def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+def __repr__(self):
+    return f"vote:{self.user_id}:{self.pitch_id}"
+
+@classmethod
+def display_upvotes(cls,id):
+    downvote=Downvote.query.filter_by(pitch_id=id).all()
+
+    return downvote
+
+class Comment(db.Model):
+    __tablename__="comments"
+    id=db.Column(db.Integer, Primary_Key=True)
+    user_id=db.Column(db.Integer, ForeignKey('users.id'))
+    pitch_id=db.Column(db.Integer, ForeignKey('pitches.id'))
+    comment=db.Column(db.Text(), nullable=False)
+
+def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+@classmethod
+def get_comments(cls,id):
+    comments=Comment.query.filter_by(pitch_id=id).all()
+
+    return comments
+
+def __repr__(self):
+    return f"comment{self.comment}"
 
 
 
