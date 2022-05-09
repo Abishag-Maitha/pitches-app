071dcf10d68cd12853b2d2860b54314aa6086292
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-09 12:46:30 +0300

    Create a profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,14 @@
 from flask_login import login_required
+from flask import render_template,request,redirect,url_for,abort
+from app import main
+from ..models import Reviews, User
 
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
