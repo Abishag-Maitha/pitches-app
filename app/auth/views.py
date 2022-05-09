fa3537f3d33c52b23e469b4ae80f4bb77225ee36
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-09 11:21:38 +0300

    create logout route which will logout the user from the application.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for, flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm,RegistrationForm
@@ -34,3 +34,9 @@ def login():
 
     title = "pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+    @auth.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+    return redirect(url_for("main.index"))
