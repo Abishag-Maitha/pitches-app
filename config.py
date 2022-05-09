58d8dbef5bebf4f627f7fd25f4afd19dbbddbb93
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-09 14:31:06 +0300

    setup the SMTP server and configure the port to use the gmail SMTP server port

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ from  dotenv import load_dotenv
 load_dotenv()
 import os
 class Config:
+       
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     '''
     General configuration parent class
     '''
@@ -10,8 +14,8 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
